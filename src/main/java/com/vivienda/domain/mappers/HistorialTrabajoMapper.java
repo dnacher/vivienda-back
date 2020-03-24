b106b75235134cdca8eb7d5b039e27448b71aa9a
DNacher <danielnacher@gmail.com>
2020-03-24 20:33:29 -0300

    se agregan algunos mappers

--- src/main/java/com/vivienda/domain/mappers/HistorialTrabajoMapper.java
+++ src/main/java/com/vivienda/domain/mappers/HistorialTrabajoMapper.java
@@ -6,4 +6,28 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class HistorialTrabajoMapper implements AbstractMapper<HistorialTrabajo, HistorialTrabajoDTO>{
+
+    @Override
+    public HistorialTrabajo mapToEntity(HistorialTrabajoDTO dto) {
+        HistorialTrabajo historialTrabajo = new HistorialTrabajo();
+        historialTrabajo.setId(dto.getId());
+        historialTrabajo.setDescripcion(dto.getDescripcion());
+        historialTrabajo.setEstado(dto.getEstado());
+        historialTrabajo.setFecha(dto.getFecha());
+        historialTrabajo.setTecnico(dto.getTecnico());
+        historialTrabajo.setTrabajo(dto.getTrabajo());
+        return historialTrabajo;
+    }
+
+    @Override
+    public HistorialTrabajoDTO mapToDTO(HistorialTrabajo entity) {
+        HistorialTrabajoDTO historialTrabajo = new HistorialTrabajoDTO();
+        historialTrabajo.setId(entity.getId());
+        historialTrabajo.setDescripcion(entity.getDescripcion());
+        historialTrabajo.setEstado(entity.getEstado());
+        historialTrabajo.setFecha(entity.getFecha());
+        historialTrabajo.setTecnico(entity.getTecnico());
+        historialTrabajo.setTrabajo(entity.getTrabajo());
+        return historialTrabajo;
+    }
 }
